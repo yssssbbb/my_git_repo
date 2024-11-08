2e2df53bf9ebe5dd8a2523bee1e8123a02563524
ys <speed100bys@gmail.com>
2024-11-08 10:27:19 +0900

    Initial commit - Add source files

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "funcs.h"
+
+int main(void){
+    print_hello();
+    print_world();
+    return 0;
+}
---
+++ src/world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <funcs.h>
+
+void print_world(void){
+    printf("World\n");
+}
